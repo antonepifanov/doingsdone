623c70e72f8628067648031b2e3637cae507c683
Антон Епифанов <antonepifanov87@gmail.com>
2024-08-10 20:20:46 +0300

    Добавляет информацию в базу данных

--- schema.sql
+++ schema.sql
@@ -7,14 +7,14 @@ USE doingsdone;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date_reg TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    email CHAR UNIQUE NOT NULL,
-    name CHAR NOT NULL,
-    password CHAR NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name CHAR NOT NULL,
+    name VARCHAR(255) NOT NULL,
     user_id INT,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
@@ -23,8 +23,8 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status TINYINT DEFAULT 0,
-    name CHAR NOT NULL,
-    file CHAR,
+    name VARCHAR(255) NOT NULL,
+    file VARCHAR(255),
     date_term DATE,
     user_id INT,
     project_id INT,
