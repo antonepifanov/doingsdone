87a13837c3eb62de8c49823ca2141c45b8395002
Антон Епифанов <antonepifanov87@gmail.com>
2024-11-17 12:57:03 +0300

    Добавляет страницу add

--- add.php
+++ add.php
@@ -28,27 +28,12 @@ else {
     if ($all_tasks_result) {
         $all_tasks = mysqli_fetch_all($all_tasks_result, MYSQLI_ASSOC);
     }
-    // Получение списка задач в зависимости от категории
-    $project = filter_input(INPUT_GET, "project");
-
-    if (isset($_GET["project"]) && !empty($_GET["project"])) {
-        $tasks_by_category_sql ="SELECT t.name AS task, t.date_term AS task_date, t.file, t.status, p.name AS category FROM tasks t JOIN projects p ON p.id = t.project_id WHERE t.user_id = 1 AND p.id = " . $_GET["project"] . "";
-        $tasks_by_category_result = mysqli_query($connection, $tasks_by_category_sql);
-
-        if ($tasks_by_category_result) {
-            $tasks_by_category = mysqli_fetch_all($tasks_by_category_result, MYSQLI_ASSOC);
-        }
-    } else {
-        $tasks_by_category = $all_tasks;
-    }
 };
 
-// HTML-код главной страницы
-$page_content = include_template("main.php", [
+// HTML-код страницы формы для добавления задачи
+$page_content = include_template("form-task.php", [
     "projects" => $projects,
     "all_tasks" => $all_tasks,
-    "tasks_by_category" => $tasks_by_category,
-    "show_complete_tasks" => $show_complete_tasks
 ]);
 
 // окончательный HTML-код
